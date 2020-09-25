050dc813d7db7cc81f61e7d87201f23712334a10
Catherine Hulshof <catherine.hulshof@gmail.com>
2020-09-25 19:42:47 -0400

    Create .Rproj and .R script

---
+++ div-dis-map.R
@@ -0,0 +1,4 @@
+## Catherine Hulshof
+## Academic Diversity Disparity Map
+## The following code uses data from the National Center for Education Statistics to create a map demonstrating the disparity between percent minority faculty and percent minority students across universities and colleges in the United States
+
